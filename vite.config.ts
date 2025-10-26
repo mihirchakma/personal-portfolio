ecc607267ad69cce3913588a5304ac45ebf1b937
Mihir Chakma <mihirchakmaths@gmail.com>
2025-10-26 20:35:32 +0530

    feat: update commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    base: '/personal-portfolio/';
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/personal-portfolio/',
       server: {
         port: 3000,
         host: '0.0.0.0',
